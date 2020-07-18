4a110a478320f60d15d61b1b3e74e12d6247c2e5
Hannah Tekié <hannahtekie@gmail.com>
2020-07-18 13:23:51 -0700

    fixed small fix on map

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -15,32 +15,35 @@ export const GoogleMapsComponent = compose(
   withScriptjs,
   withGoogleMap
 )((props) => {
-  const [markers, setMarkers] = useState([]);
 
 
-  props.addresses.forEach((addr) => {
-    Axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
-      params: {
-        address: addr,
-        key: process.env.REACT_APP_GOOGLE_MAPS_KEY
-      }
-    }).then((resp) => {
-      const coordinates = resp.data.results[0].geometry.location
-      setMarkers(
-      [...markers, <Marker position={{ lat: coordinates.lat, lng: coordinates.lng }} />])
-      console.log(markers)
+  const getMarkersFromAddrs = () => {
+    let array = []
+
+    props.addresses.forEach((addr) => {
+      Axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+        params: {
+          address: addr,
+          key: process.env.REACT_APP_GOOGLE_MAPS_KEY
+        }
+      }).then((resp) => {
+        const coordinates = resp.data.results[0].geometry.location
+        array.push(<Marker position={{ lat: coordinates.lat, lng: coordinates.lng }} />)
+
+      })
     })
-  })
+    return array
+  }
 
-  
+  const [markers, setMarkers] = useState(getMarkersFromAddrs());
+  console.log(markers)
+
 
   return (<GoogleMap
     defaultZoom={8}
     defaultCenter={{ lat: 47.6487172, lng: -122.385203 }}
   >
-    {props.isMarkerShown && markers }
-
-
+    {props.isMarkerShown && markers}
   </GoogleMap>)
 }
 );
